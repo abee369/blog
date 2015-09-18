3f573dbca7719968f54b4813f5eff75363657d8d
abee369 <abee369@hotmail.com>
2015-09-17 21:42:03 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+	validates :username, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :password, presence: true, confirmation: true
+end
